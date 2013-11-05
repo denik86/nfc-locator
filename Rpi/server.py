d324662b15003676d4c713ba9cff6dadb18ef8e7
mick89.mac@gmail.com <mick89.mac@gmail.com@9c0ec5b0-c9d3-92da-7579-03812a529fb4>
2013-11-05 10:43:47 +0000

    aggiunto l'invio a seriale da Rpi ad arduino

--- Rpi/server.py
+++ Rpi/server.py
@@ -1,17 +1,20 @@
 #!/usr/bin/env python
 
 import socket
+import serial
 
 host = ''
 port = 9093
 backlog = 5
 size = 1024
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+ser = serial.Serial('/dev/tty.usbserial', 9600)
 s.bind((host,port))
 s.listen(backlog)
 while 1:
 	client, address = s.accept()
 	data = client.recv(size)
 	if data:
+		ser.write(data)
 		client.send(data)
 	client.close()
