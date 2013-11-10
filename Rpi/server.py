96b51debe00681f0062d9051ba3c71394e84ba25
mick89.mac@gmail.com <mick89.mac@gmail.com@9c0ec5b0-c9d3-92da-7579-03812a529fb4>
2013-11-10 10:51:18 +0000

    Aggiornate le componenti finali, anche se non completamente.

--- Rpi/server.py
+++ Rpi/server.py
@@ -2,13 +2,16 @@
 
 import socket
 import serial
+from time import sleep
 
 host = ''
 port = 9093
 backlog = 5
 size = 1024
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-ser = serial.Serial('/dev/tty.usbmodem1451', 9600)
+ser = serial.Serial('/dev/tty.usbmodem1411', 9600)
+sleep(3)
+ser.write('s');
 s.bind((host,port))
 s.listen(backlog)
 while 1:
