963bd2d8edd2ee6ff6283daac16123ffec16dcac
mick89.mac@gmail.com <mick89.mac@gmail.com@9c0ec5b0-c9d3-92da-7579-03812a529fb4>
2013-11-13 21:50:22 +0000

    Aggiunto un link alle impostazioni in Reader.java. Modificata l'icona "OPEN".

--- trunk/client/NFCLocator/src/it/mm/nfclocator/Configuration.java
+++ trunk/client/NFCLocator/src/it/mm/nfclocator/Configuration.java
@@ -19,116 +19,13 @@ public class Configuration extends Activity {
 		Fragment pref = new SettingFragment();
 		getFragmentManager().beginTransaction().replace(android.R.id.content, pref).commit();
 		
-		/*final Button saveButton = (Button) findViewById(R.id.saveButton);
-		final EditText text = (EditText) findViewById(R.id.pswd	);
-		
-		saveButton.setOnClickListener(new View.OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				String FILENAME = "configuration";
-				try {
-					FileOutputStream fos = openFileOutput(FILENAME, Context.MODE_PRIVATE);
-					fos.write(text.getText().toString().getBytes());
-					fos.close();
-					Context context = getApplicationContext();
-					Toast toast = Toast.makeText(context, "saved!", Toast.LENGTH_SHORT);
-					toast.show();
-				} catch (Exception e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			}
-		});*/
-		
-		/*Button read = (Button) findViewById(R.id.read);
-		writeLabel = (TextView) findViewById(R.id.write);
-		
-		myReceiver = new MyReceiver();
-		registerReceiver(myReceiver, new IntentFilter(NfcAdapter.ACTION_NDEF_DISCOVERED));
-		
-		read.setOnClickListener(new View.OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				readTag();
-				
-			}
-		});*/
 	}
-	
-	/*private void readTag () {
-		Intent nfcintent = getIntent();
-		Tag myTag = (Tag) nfcintent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
-		
-		Ndef ndefTag = Ndef.get(myTag);
-		int size = ndefTag.getMaxSize();
-		boolean writable = ndefTag.isWritable();
-		String type = ndefTag.getType();
-		
-		NdefMessage ndefMesg = ndefTag.getCachedNdefMessage();
-		NdefRecord[] ndefRecords = ndefMesg.getRecords();
-		int len = ndefRecords.length;
-		for (int i=0; i<len; i++) {
-			byte[] payload = ndefRecords[i].getPayload();
-			for(int j=0; j<payload.length; j++) {
-				System.out.println(payload[j]);
-			}
-		}
-	}*/
-	
-	/*protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-		super.onActivityResult(requestCode, resultCode, data);
-		
-		//Intent nfcintent = getIntent(); // ???
-		Tag myTag = (Tag) data.getParcelableExtra(NfcAdapter.EXTRA_TAG);
-		
-		Ndef ndefTag = Ndef.get(myTag);
-		int size = ndefTag.getMaxSize();
-		boolean writable = ndefTag.isWritable();
-		String type = ndefTag.getType();
-		
-		NdefMessage ndefMesg = ndefTag.getCachedNdefMessage();
-		NdefRecord[] ndefRecords = ndefMesg.getRecords();
-		int len = ndefRecords.length;
-		for (int i=0; i<len; i++) {
-			byte[] payload = ndefRecords[i].getPayload();
-			for(int j=0; j<payload.length; j++) {
-				System.out.println(payload[j]);
-			}
-		}
-		
-	}*/
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.activity_configuration, menu);
+		// No menu for this activity
+		//getMenuInflater().inflate(R.menu.activity_configuration, menu);
 		return true;
 	}
-	
-	/*public class MyReceiver extends BroadcastReceiver {
-
-		@Override
-		public void onReceive(Context arg0, Intent data) {
-			Tag myTag = (Tag) data.getParcelableExtra(NfcAdapter.EXTRA_TAG);
-			
-			Ndef ndefTag = Ndef.get(myTag);
-			int size = ndefTag.getMaxSize();
-			boolean writable = ndefTag.isWritable();
-			String type = ndefTag.getType();
-			
-			NdefMessage ndefMesg = ndefTag.getCachedNdefMessage();
-			NdefRecord[] ndefRecords = ndefMesg.getRecords();
-			int len = ndefRecords.length;
-			for (int i=0; i<len; i++) {
-				byte[] payload = ndefRecords[i].getPayload();
-				for(int j=0; j<payload.length; j++) {
-					System.out.println(payload[j]);
-				}
-			}
-		}
-		
-	}*/
 
 }
